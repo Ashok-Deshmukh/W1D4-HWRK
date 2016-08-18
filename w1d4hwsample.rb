ce5d39e854b9aff92dbea81750b7bd9cb0ce467e
Ashok-Deshmukh <Ashok-Deshmukh@users.noreply.github.com>
2016-08-18 01:03:38 -0400

    This is the commit for w1d4 homework

---
+++ w1d4hwsample.rb
@@ -0,0 +1,7 @@
+#Initialize git
+
+#Make a new folder on your computer. cd into it and save a sample file (you can put whatever you want in the file).
+
+#Run git status in terminal. You should get an error. Read the error and see if it makes sense.
+
+#Now initialize git with git init. Run git status again and compare.
